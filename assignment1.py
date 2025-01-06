9431ccd93608f5326e51f70dbcf40d7a5df6a862
matt-fang <matthewfang.calif@gmail.com>
2025-01-05 21:26:30 -0800

    Revised from posted solution.

--- assignment1.py
+++ assignment1.py
@@ -1,7 +1,10 @@
 """Give your name and get a friendly greeting."""
 
 def main():
-    """Take user's name and print greeting."""
+    """Take user's name and print greeting.
+
+    :return: None
+    """
     name = input("Please enter your name: ")
     print ("Hi " + name + ", let's explore some historical temperatures.")
 

4f019e3a58a633cbca7c685694b55c2ccddc7909
matt-fang <matthewfang.calif@gmail.com>
2025-01-05 22:38:14 -0800

    Revised module docstring, and cleaned up menu() using an empty return.

--- asst-3/Asst3.py
+++ asst-3/Asst3.py
@@ -1,4 +1,8 @@
-"""Give your name and get a friendly greeting."""
+"""Download and display historical temperature information.
+
+This package allows the user to download historical temperature
+information using the open-meteo API, and display summary information.
+"""
 
 
 def main():
@@ -42,27 +46,28 @@ def menu():
         option = int(option)
     except ValueError:
         print("please enter a numerical value")
+        return
+
+    if option == 1:
+        print("option 1 is not functional yet")
+    elif option == 2:
+        print("option 2 is not functional yet")
+    elif option == 3:
+        print("option 3 is not functional yet")
+    elif option == 4:
+        print("option 4 is not functional yet")
+    elif option == 5:
+        print("option 5 is not functional yet")
+    elif option == 6:
+        print("option 6 is not functional yet")
+    elif option == 7:
+        print("option 7 is not functional yet")
+    elif option == 8:
+        print("option 8 is not functional yet")
+    elif option == 9:
+        print("option 9 is not functional yet")
     else:
-        if option == 1:
-            print("option 1 is not functional yet")
-        elif option == 2:
-            print("option 2 is not functional yet")
-        elif option == 3:
-            print("option 3 is not functional yet")
-        elif option == 4:
-            print("option 4 is not functional yet")
-        elif option == 5:
-            print("option 5 is not functional yet")
-        elif option == 6:
-            print("option 6 is not functional yet")
-        elif option == 7:
-            print("option 7 is not functional yet")
-        elif option == 8:
-            print("option 8 is not functional yet")
-        elif option == 9:
-            print("option 9 is not functional yet")
-        else:
-            print("your choice is out of range")
+        print("your choice is out of range")
 
 
 if __name__ == "__main__":

d8979f0215ad9cbbec704361a4c518fa3a280138
matt-fang <matthewfang.calif@gmail.com>
2025-01-05 22:00:02 -0800

    Organized files and submitted assignment 2.

---
+++ asst-2/Asst2.py
@@ -0,0 +1,15 @@
+"""Give your name and get a friendly greeting."""
+
+
+def main():
+    """Take user's name and print greeting.
+
+    :return: None
+    """
+
+    name = input("Please enter your name: ")
+    print(f"Hi {name}, let's explore some historical temperatures.")
+
+
+if __name__ == "__main__":
+    main()
